5de70de163f4bf9f3fede02c163b0520c432dbe1
pchannary <phachannary101@gmail.com>
2024-02-15 13:18:50 +0700

    change app name on flash screen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:test/presentation/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _SplashScreenState createState() => _SplashScreenState();
 }
 
@@ -28,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
     Timer(const Duration(seconds: 5), () { 
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (context) => LoginScreen(), // Navigate to LoginScreen
+          builder: (context) => const LoginScreen(), // Navigate to LoginScreen
         ),
       );
     });
@@ -61,7 +62,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
                     ),
                     // const SizedBox(height: 1),
                     const Text(
-                      'BookBound G4',
+                      'FlyBook G4',
                       style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
                     ),
                   ],
